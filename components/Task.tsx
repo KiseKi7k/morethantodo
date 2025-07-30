10d8b45baa695824e6715525615974b32e84f516
Pornchai Wannasuk <topzanahee@gmail.com>
2025-07-30 11:23:57 +0700

    Added: Task move dropdown

--- components/Task.tsx
+++ components/Task.tsx
@@ -1,32 +1,91 @@
 import { TaskType } from "@/lib/mockData";
-import { EllipsisVertical, Pencil, Trash, Trash2 } from "lucide-react";
+import {
+  ArrowRightFromLine,
+  ChevronRight,
+  EllipsisVertical,
+  Pencil,
+  Trash,
+} from "lucide-react";
 import React from "react";
-import { PopoverTrigger, Popover, PopoverContent } from "./ui/popover";
-import { Separator } from "./ui/separator";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuPortal,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import {
+  DropdownMenuItem,
+  DropdownMenuSubTrigger,
+} from "@radix-ui/react-dropdown-menu";
 
 const Task = ({ task }: { task: TaskType }) => {
+  const handleMoveTask = () => {};
+
+  const handleEdit = () => {};
+
+  const handleDelete = () => {};
+
+  const isMoveItemDisabled = (status: string) => {
+    return task.status === status;
+  };
+
+  const MoveDropdownItem = ({
+    status,
+    text,
+  }: {
+    status: string;
+    text: string;
+  }) => {
+    return (
+      <DropdownMenuItem
+        onClick={() => handleMoveTask()}
+        disabled={isMoveItemDisabled(status)}
+        className={`task-dropdown-item !p-1 ${
+          isMoveItemDisabled(status)
+            ? "hover:!bg-accent-1/30 text-gray-700"
+            : ""
+        }`}
+      >
+        {text}
+      </DropdownMenuItem>
+    );
+  };
+
   return (
     <div className="relative w-[250px] aspect-[3/1] bg-background border-6 border-accent-3 rounded-xl p-2">
       <p className="text-xl overflow-clip">{task.title}</p>
       <div className="absolute top-2 right-[-4px] cursor-pointer">
-        <Popover>
-          <PopoverTrigger className="cursor-pointer">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
             <EllipsisVertical />
-          </PopoverTrigger>
-          <PopoverContent className="w-30 p-2">
-            <div className="flex flex-col gap-2 justify-center">
-              <div className="group flex flex-row gap-2 p-1 items-center rounded-sm cursor-pointer transition-all duration-200 ease-out hover:bg-accent-1/60 hover:scale-105">
-                <Pencil size={15} />
-                Edit
-              </div>
-              <Separator />
-              <div className="group flex flex-row gap-2 p-1 items-center rounded-sm cursor-pointer transition-all duration-200 ease-out hover:bg-accent-1/60 hover:scale-105">
-                <Trash size={15} />
-                Delete
-              </div>
-            </div>
-          </PopoverContent>
-        </Popover>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="p-2 bg-background border-2 border-accent-3">
+            <DropdownMenuSub>
+              <DropdownMenuSubTrigger className="task-dropdown-item">
+                <ArrowRightFromLine size={15} />
+                Move
+                <ChevronRight size={15} className="ml-auto" />
+              </DropdownMenuSubTrigger>
+              <DropdownMenuPortal>
+                <DropdownMenuSubContent className="p-2 bg-background border-2 border-accent-3">
+                  <MoveDropdownItem status="pending" text="Pending" />
+                  <MoveDropdownItem status="inProgress" text="In Progress" />
+                  <MoveDropdownItem status="completed" text="Completed" />
+                </DropdownMenuSubContent>
+              </DropdownMenuPortal>
+            </DropdownMenuSub>
+            <DropdownMenuItem className="task-dropdown-item">
+              <Pencil size={15} />
+              Edit
+            </DropdownMenuItem>
+            <DropdownMenuItem className="task-dropdown-item">
+              <Trash size={15} />
+              Delete
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </div>
   );
